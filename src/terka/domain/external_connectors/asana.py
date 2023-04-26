7e821a8587a4d7aacff0e9ce76db52d838363775
Andrei Markin <andrey.markin.ppc@gmail.com>
2023-04-26 23:47:05 +0400

    Add sync date for asana connector

--- src/terka/domain/external_connectors/asana.py
+++ src/terka/domain/external_connectors/asana.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from dataclasses import dataclass
 
 
@@ -6,9 +7,11 @@ class AsanaTask:
     id: int
     project: int
     asana_task_id: str
+    sync_date: datetime
 
 
 @dataclass
 class AsanaProject:
     id: int
     asana_project_id: str
+    sync_date: datetime
