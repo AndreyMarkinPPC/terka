ae7f1d23e2eae8e697d35c160649aee11942d251
Andrei Markin <andrey.markin.ppc@gmail.com>
2023-04-26 23:51:49 +0400

    Bump version to 1.7.0

--- src/setup.py
+++ src/setup.py
@@ -6,7 +6,7 @@ HERE = pathlib.Path(__file__).parent
 
 setup(
     name="terka",
-    version="1.6.5",
+    version="1.7.0",
     description="CLI utility for creating and managing tasks in a terminal",
     # long_description=README,
     long_description_content_type="text/markdown",
