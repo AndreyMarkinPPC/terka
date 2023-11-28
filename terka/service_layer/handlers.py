b38e577b3a87580c3065f80a01fe70b382d46bd2
Andrei Markin <andrey.markin.ppc@gmail.com>
2023-11-28 23:17:56 +0400

    Bump version to 2.0.8

--- terka/service_layer/handlers.py
+++ terka/service_layer/handlers.py
@@ -172,11 +172,12 @@ class TaskCommandHandlers:
             new_task = models.task.Task(**asdict(cmd))
             uow.tasks.add(new_task)
             uow.flush()
-            uow.published_events.append(events.TaskCreated(new_task.id))
+            task_created_event = events.TaskCreated(new_task.id)
+            uow.published_events.append(task_created_event)
             uow.commit()
             TaskCommandHandlers._process_extra_args(new_task.id, context, uow)
             handler.publisher.publish("Topic",
-                                      events.TaskCompleted(new_task.id))
+                                      task_created_event)
             handler.printer.console.print_new_object(new_task)
 
     @register(cmd=_commands.UpdateTask)
