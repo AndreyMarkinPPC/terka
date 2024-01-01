18506c26553937d4e8a96c2a2a8aeb7a83ef3d96
Andrei Markin <andrey.markin.ppc@gmail.com>
2024-01-02 00:44:28 +0400

    Add extra event types in event_history

--- terka/domain/entities/event_history.py
+++ terka/domain/entities/event_history.py
@@ -11,6 +11,12 @@ class EventType(Enum):
     DESCRIPTION = auto()
     PRIORITY = auto()
     ASSIGNEE = auto()
+    ADDED_TO_SPRINT = auto()
+    REMOVED_FROM_SPRINT = auto()
+    ADDED_TO_EPIC = auto()
+    REMOVED_FROM_EPIC = auto()
+    ADDED_TO_STORY = auto()
+    REMOVED_FROM_STORY = auto()
 
 
 @dataclass
@@ -21,9 +27,15 @@ class TaskEvent:
     new_value: str
     date: datetime = datetime.now()
 
-    def __post__init__(self) -> None:
-        if self.event_type.upper() not in [e.name for e in EventType]:
-            raise ValueError(f"{self.event_type} is invalid EventType")
+    def __post_init__(self) -> None:
+        try:
+            EventType[self.type]
+        except KeyError as e:
+            raise ValueError(
+                f"{self.type} is invalid EventType for Task") from None
+        # TODO: Select better name
+        self.syncable = self.type in ("STATUS", "DUE_DATE", "NAME",
+                                        "PROJECT", "DESCRIPTION", "PRIORITY")
 
 
 @dataclass
@@ -34,6 +46,6 @@ class ProjectEvent:
     new_value: str
     date: datetime = datetime.now()
 
-    def __post__init__(self) -> None:
+    def __post_init__(self) -> None:
         if self.event_type.upper() not in [e.name for e in EventType]:
             raise ValueError(f"{self.event_type} is invalid EventType")
