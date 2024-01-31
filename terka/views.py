0845cb58c5afd4b7ecc9cff4ba4b1d4613d40ae4
Andrei Markin <andrey.markin.ppc@gmail.com>
2024-02-01 00:07:09 +0400

    Clean up views module

--- terka/views.py
+++ terka/views.py
@@ -1,28 +1,15 @@
 from __future__ import annotations
-from copy import copy
-import pandas as pd
-import numpy as np
-from datetime import datetime, timedelta
-from sqlalchemy import text
+from datetime import datetime
 
 from terka.domain import entities
 
 
-def n_days_ago(n: int) -> str:
-    return (datetime.today() - timedelta(n)).strftime("%Y-%m-%d")
-
-
-def projects(uow):
+def projects(uow) -> list[dict]:
     with uow:
         return [t.to_dict() for t in uow.tasks.list(entities.project.Project)]
 
 
-def projects_id_to_name_mapping(uow) -> dict[int, str]:
-    with copy(uow) as uow:
-        return {t.id: t.name for t in uow.tasks.list(entities.project.Project)}
-
-
-def project(uow, project_id: int):
+def project(uow, project_id: int) -> dict:
     with uow:
         if not (project := uow.tasks.get_by_id(entities.project.Project,
                                                project_id)):
@@ -35,22 +22,22 @@ def project(uow, project_id: int):
         return result
 
 
-def project_tasks(uow, project_id: int):
+def project_tasks(uow, project_id: int) -> list[dict]:
     with uow:
         if not (project := uow.tasks.get_by_id(entities.project.Project,
                                                project_id)):
-            return {}
+            return []
         if tasks := project.tasks:
             return [task.to_dict() for task in project.tasks]
-        return {}
+        return []
 
 
-def tasks(uow):
+def tasks(uow) -> list[dict]:
     with uow:
         return [t.to_dict() for t in uow.tasks.list(entities.task.Task)]
 
 
-def task(uow, task_id: int):
+def task(uow, task_id: int) -> dict:
     with uow:
         if not (task := uow.tasks.get_by_id(entities.task.Task, task_id)):
             return {}
@@ -61,13 +48,13 @@ def task(uow, task_id: int):
         return result
 
 
-def task_commentaries(uow, task_id: int) -> dict:
+def task_commentaries(uow, task_id: int) -> list[dict]:
     with uow:
         if not (task := uow.tasks.get_by_id(entities.task.Task, task_id)):
-            return {}
+            return []
         if commentaries := task.commentaries:
             return [comment.to_dict() for comment in commentaries]
-        return {}
+        return []
 
 
 def workspaces(uow) -> list[dict]:
@@ -77,15 +64,7 @@ def workspaces(uow) -> list[dict]:
         ]
 
 
-def workspaces_id_to_name_mapping(uow) -> dict[int, str]:
-    with copy(uow) as uow:
-        return {
-            t.id: t.name
-            for t in uow.tasks.list(entities.workspace.Workspace)
-        }
-
-
-def workspace(uow, workspace_id: int):
+def workspace(uow, workspace_id: int) -> dict:
     with uow:
         if not (workspace := uow.tasks.get_by_id(entities.workspace.Workspace,
                                                  workspace_id)):
@@ -97,22 +76,22 @@ def workspace(uow, workspace_id: int):
         return result
 
 
-def workspace_projects(uow, workspace_id: int):
+def workspace_projects(uow, workspace_id: int) -> list[dict]:
     with uow:
         if not (workspace := uow.tasks.get_by_id(entities.workspace.Workspace,
                                                  workspace_id)):
-            return {}
+            return []
         if projects := workspace.projects:
             return [project.to_dict() for project in workspace.projects]
-        return {}
+        return []
 
 
-def epics(uow):
+def epics(uow) -> list[dict]:
     with uow:
         return [t.to_dict() for t in uow.tasks.list(entities.epic.Epic)]
 
 
-def epic(uow, epic_id: int):
+def epic(uow, epic_id: int) -> dict:
     with uow:
         if not (epic := uow.tasks.get_by_id(entities.epic.Epic, epic_id)):
             return {}
@@ -123,21 +102,21 @@ def epic(uow, epic_id: int):
         return result
 
 
-def epic_tasks(uow, epic_id: int):
+def epic_tasks(uow, epic_id: int) -> list[dict]:
     with uow:
         if not (epic := uow.tasks.get_by_id(entities.epic.Epic, epic_id)):
-            return {}
+            return []
         if tasks := epic.tasks:
             return [task.tasks.to_dict() for task in epic.tasks]
-        return {}
+        return []
 
 
-def stories(uow):
+def stories(uow) -> list[dict]:
     with uow:
         return [t.to_dict() for t in uow.tasks.list(entities.story.Story)]
 
 
-def story(uow, story_id: int):
+def story(uow, story_id: int) -> dict:
     with uow:
         if not (story := uow.tasks.get_by_id(entities.story.Story, story_id)):
             return {}
@@ -148,21 +127,21 @@ def story(uow, story_id: int):
         return result
 
 
-def story_tasks(uow, story_id: int):
+def story_tasks(uow, story_id: int) -> list[dict]:
     with uow:
         if not (story := uow.tasks.get_by_id(entities.story.Story, story_id)):
-            return {}
+            return []
         if tasks := story.tasks:
             return [task.tasks.to_dict() for task in story.tasks]
-        return {}
+        return []
 
 
-def sprints(uow):
+def sprints(uow) -> list[dict]:
     with uow:
         return [t.to_dict() for t in uow.tasks.list(entities.sprint.Sprint)]
 
 
-def sprint(uow, sprint_id: int):
+def sprint(uow, sprint_id: int) -> dict:
     with uow:
         if not (sprint := uow.tasks.get_by_id(entities.sprint.Sprint,
                                               sprint_id)):
@@ -174,67 +153,38 @@ def sprint(uow, sprint_id: int):
         return result
 
 
-def sprint_tasks(uow, sprint_id: int):
+def sprint_tasks(uow, sprint_id: int) -> list[dict]:
     with uow:
         if not (sprint := uow.tasks.get_by_id(entities.sprint.Sprint,
                                               sprint_id)):
-            return {}
+            return []
         if tasks := sprint.tasks:
             return [task.tasks.to_dict() for task in sprint.tasks]
-        return {}
+        return []
 
 
-def users(uow):
+def users(uow) -> list[dict]:
     with uow:
         return [t.to_dict() for t in uow.tasks.list(entities.user.User)]
 
 
-def users_id_to_name_mapping(uow) -> dict:
-    with copy(uow) as uow:
-        return {t.id: t.name for t in uow.tasks.list(entities.user.User)}
-
-
-def user(uow, user_id: int):
+def user(uow, user_id: int) -> dict:
     with uow:
         if not (user := uow.tasks.get_by_id(entities.user.User, user_id)):
             return {}
         return user.to_dict()
 
 
-def tags(uow):
+def tags(uow) -> list[dict]:
     with uow:
         return [t.to_dict() for t in uow.tasks.list(entities.tag.BaseTag)]
 
 
-def tag(uow, tag_id: int):
+def tag(uow, tag_id: int) -> dict:
     with uow:
         if not (tag := uow.tasks.get_by_id(entities.tag.BaseTag, tag_id)):
             return {}
         return tag.to_dict()
-
-
-def time_spent(session,
-               tasks: list[entities.task.Task] | None,
-               start_date: str = n_days_ago(7),
-               end_date: str = n_days_ago(-1),
-               excluded_tasks_only: bool = False) -> list[dict[str, str]]:
-    tasks = ",".join([str(task.id) for task in tasks])
-    query = f"""
-    SELECT
-        STRFTIME("%Y-%m-%d", creation_date) AS date,
-        SUM(time_spent_minutes) AS time_spent_hours
-    FROM time_tracker_entries
-    WHERE
-        creation_date >= "{start_date}"
-        AND creation_date <= "{end_date}"
-    """
-    if excluded_tasks_only:
-        query += f" AND task NOT IN ({tasks})"
-    else:
-        query += f" AND task IN ({tasks})"
-    query += "GROUP BY 1"
-    results = session.execute(text(query))
-    return [dict(r) for r in results]
 
 
 def sprint_task_ids(session,
@@ -288,21 +238,3 @@ def external_connectors_asana_tasks(
         }
         for r in results
     }
-
-
-def status_changes(session, sprint_id: int):
-    query = f"""
-        SELECT DISTINCT T.id AS task,
-        T.status AS current_status,
-        STRFTIME("%Y-%m-%d", E.date) AS date,
-        FIRST_VALUE(E.old_value) OVER (PARTITION BY T.id, STRFTIME("%Y-%m-%d", E.date) ORDER by E.date RANGE BETWEEN UNBOUNDED PRECEDING AND
-                UNBOUNDED FOLLOWING) AS first_status_for_date,
-        LAST_VALUE(E.new_value) OVER (PARTITION BY T.id, STRFTIME("%Y-%m-%d", E.date) ORDER by E.date RANGE BETWEEN UNBOUNDED PRECEDING AND
-                UNBOUNDED FOLLOWING) AS last_status_for_date
-        FROM tasks AS T
-        LEFT JOIN (SELECT * FROM task_events WHERE type = "STATUS") AS E
-            ON T.id = E.task
-        WHERE T.id IN (SELECT task FROM sprint_tasks WHERE sprint = {sprint_id})
-
-        """
-    return pd.read_sql_query(query, session.get_bind()).replace({np.nan: None})
