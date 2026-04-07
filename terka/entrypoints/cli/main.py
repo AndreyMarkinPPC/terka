6adc60313b1358aca20c51edd41eb2df6abe3fd1
Andrei Markin <amarkin@google.com>
2026-04-07 16:53:21 +0400

    chore: extract version into a dedicated command

--- terka/entrypoints/cli/main.py
+++ terka/entrypoints/cli/main.py
@@ -54,91 +54,22 @@ def init_db(home_dir):
   return engine
 
 
-def _version_callback(show_version: bool) -> None:
-  if show_version:
-    print(f'terka version {__version__}')
-    raise typer.Exit()
+@typer_app.command()
+def version():
+  """Display app version."""
+  print(f'terka version {__version__}')
+  raise typer.Exit()
 
 
 @typer_app.callback()
 def main(
   ctx: typer.Context,
-  version: Annotated[
-    bool,
-    typer.Option(
-      help='Display library version',
-      callback=_version_callback,
-      is_eager=True,
-      expose_value=False,
-    ),
-  ] = False,
 ):
   config = load_config(HOME_DIR)
   ctx.obj = {}
   ctx.obj['bus'] = bootstrap.bootstrap(
     start_orm=True, uow=unit_of_work.SqlAlchemyUnitOfWork(DB_URL), config=config
   )
-
-
-# parser = argparse.ArgumentParser()
-# parser.add_argument('command', nargs='?')
-# parser.add_argument('entity', nargs='?')
-# parser.add_argument('--log', '--loglevel', dest='loglevel', default='info')
-# parser.add_argument('-v', '--version', dest='version', action='store_true')
-# args = parser.parse_known_args()
-# args, kwargs = args
-# console = Console()
-# command, entity = args.command, args.entity
-# home_dir = os.path.expanduser('~')
-# if args.version:
-#   print(f'terka version {__version__}')
-#   exit()
-# if args.command == 'config':
-#   console.print(services.get_config())
-#   exit()
-# if args.command == 'init':
-#   services.ServiceCommandHander(home_dir, None, None).execute(
-#     command, None, None
-#   )
-
-# file_handler = logging.FileHandler(filename=f'{home_dir}/.terka/terka.log')
-# stdout_handler = logging.StreamHandler(stream=sys.stdout)
-# log_handlers = [file_handler, stdout_handler]
-# logging.basicConfig(
-#   format='[%(asctime)s][%(name)s][%(levelname)s] %(message)s',
-#   handlers=log_handlers,
-#   level=args.loglevel.upper(),
-#   datefmt='%Y-%m-%d %H:%M:%S',
-# )
-# logger = logging.getLogger(__name__)
-
-# config = load_config(home_dir)
-# task_id = config.get('task_id')
-# project_name = config.get('project_name')
-# if task_id or project_name:
-#   focus_type = 'task' if task_id else 'project'
-#   logger.warning('Using terka in focus mode')
-#   logger.warning(f'Current focus is {focus_type}: {task_id or project_name}')
-
-# task_dict = format_task_dict(config, args, kwargs)
-# logger.debug(task_dict)
-
-# service_command_handler = services.ServiceCommandHander(
-#   home_dir, config, console
-# )
-# service_command_handler.execute(command, entity, task_dict)
-
-# bus = bootstrap.bootstrap(
-#   start_orm=True, uow=unit_of_work.SqlAlchemyUnitOfWork(DB_URL), config=config
-# )
-# queue = []
-# queue.append({'command': command, 'entity': entity, 'task_dict': task_dict})
-# while queue:
-#   cmd_dict = queue.pop()
-#   try:
-#     handlers.CommandHandler(bus).execute(**cmd_dict)
-#   except exceptions.TerkaRefreshException:
-#     queue.append(cmd_dict)
 
 
 if __name__ == '__main__':
